221adfa3aa063954195b2fe9a499a8a76bd89606
felipe <profissionalf.andre@gmail.com>
2024-08-14 21:07:37 -0300

    feat: add more endpoints + tests with pytest

--- learning_fast_api/app.py
+++ learning_fast_api/app.py
@@ -1,12 +1,62 @@
-from fastapi import FastAPI
-
 from http import HTTPStatus
-from learning_fast_api.schemas import Message
 
+from fastapi import FastAPI, HTTPException
+
+from learning_fast_api.schemas import (
+    Message,
+    UserDB,
+    UserPublicSchema,
+    UserSchema,
+    UsersList,
+)
 
 app = FastAPI()
+database = []
 
 
-@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {"message": "Olá Mundo!"}
+    return {'message': 'Olá Mundo!'}
+
+
+@app.post(
+    '/users/',
+    response_model=UserPublicSchema,
+    status_code=HTTPStatus.CREATED,
+)
+def create_user(user: UserSchema) -> UserDB:
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', response_model=UsersList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}/', response_model=UserSchema)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}/', response_model=Message)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,74 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from learning_fast_api.app import app
-
-
-def test_read_root_should_return_OK_and_hello_world():
-    client = TestClient(app)
-
-    response = client.get("/")
+def test_read_root_should_return_OK_and_hello_world(client):
+    response = client.get('/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"message": "Olá Mundo!"}
+    assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user(client):
+    data = {
+        'username': 'user',
+        'email': 'email@gmail.com',
+        'password': 'password',
+    }
+    expected_response = {
+        'id': 1,
+        'username': 'user',
+        'email': 'email@gmail.com',
+    }
+
+    response = client.post('/users', json=data)
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == expected_response
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'user',
+                'email': 'email@gmail.com',
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'user',
+            'email': 'email@gmail.com',
+            'password': 'password',
+            'id': 1,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_update_user_when_id_not_found(client):
+    response = client.put(
+        '/users/333',
+        json={
+            'username': 'user',
+            'email': 'email@gmail.com',
+            'password': 'password',
+            'id': 1,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted'}
